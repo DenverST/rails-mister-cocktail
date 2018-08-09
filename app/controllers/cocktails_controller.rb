e6aa5933d2d64c7b9a2d7c2fd228c946a216e54b
Denver Stove <dtstove@gmail.com>
2018-08-09 16:45:26 +1000

    All green, just need to style

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -21,10 +21,18 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to cocktails_path
+  end
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :ingredients, :doses)
+    params.require(:cocktail).permit(:name, :doses)
   end
 
 end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,40 @@
+class DosesController < ApplicationController
+
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params["cocktail_id"])
+  end
+
+  def create
+    # @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params["cocktail_id"])
+    # @dose.cocktail = @cocktail
+    @dose = @cocktail.doses.new(dose_params)
+    # raise
+    # @ingredient = Ingredient.find(dose_params["ingredient_id"])
+    # @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
